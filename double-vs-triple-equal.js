28108bbf42968dcdd3c2539699a057e06a9d1bb8
unknown <you@example.com>
2021-02-01 23:44:13 +0600

    double vs triple cleared

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,77 @@
+INTERVEW
+
+const first= 2;
+const second=5;
+if(first ==second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+output:false
+const first= 2;
+const second="2";
+if(first ==second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+output;true
+
+const first= 2;
+const second= "2";
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+//output:false
+
+const first= 2;
+const second= true;
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+output:false
+const first= 1;
+const second= true;
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+output:true
+
+const first= 2;
+const second= false;
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+output:false
+const first= 0;
+const second= true;
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+false
+
+const first= 0;
+const second= true;
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
