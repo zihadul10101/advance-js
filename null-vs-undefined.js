d380636f8de44f256238523e086ec38b36fa5796
unknown <you@example.com>
2021-02-01 23:14:14 +0600

    null vs undefined cleared

---
+++ null-vs-undefined.js
@@ -0,0 +1,41 @@
+undefined
+01.
+let pakhi;
+console.log(pakhi);
+02.
+function add(num1,num2){
+    console.log(num1+num2);
+}
+const result=add(13,82);
+console.log(result);
+
+03.
+
+    function add(num1,num2){
+            console.log(num1+num2);
+            return
+        }
+         const result=add(13,82);
+        console.log(result);
+
+   04.
+   function add(num1,num2){
+            console.log(num2);
+            return
+        }
+         const result=add(13);
+        console.log(result);
+
+05.
+const premik={name:"smart dude",phone:45865};
+console.log(premik.bf);
+        
+   06.
+   let fun =undefined; 
+   console.log(fun);
+07.
+let ages=[2,5,9];
+console.log(ages[11]);
+null
+
+
