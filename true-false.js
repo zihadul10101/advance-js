06af8b6414aa067a4a001571284024ebce835d0e
unknown <you@example.com>
2021-02-01 22:50:45 +0600

    true or false

---
+++ true-false.js
@@ -0,0 +1,21 @@
+//const age=0;
+
+// if(age>0){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
+//const name="solaiman";
+//let name=null;
+//falsy:0,"",null,NaN ,undefined,false
+//truthy:'0' " ",
+let name =0;
+//console.log(name);
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
