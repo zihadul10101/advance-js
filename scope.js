83dc2df0ca35edb8517149efa159b9f8bf4b4373
unknown <you@example.com>
2021-02-02 20:11:58 +0600

    closure and scope complit

---
+++ scope.js
@@ -0,0 +1,23 @@
+let bonus=20;
+
+function sum(first,second){
+    let result=first+second +bonus;
+    
+    if(result>9){
+       // const mood="happy";
+        //console.log(mood);
+        let mood ="happy";
+        mood="fishy";
+        mood="cranky"
+       // console.log(mood);
+    }
+console.log(day);
+//let day="faiday";
+const day="fariday";
+    return result;
+   // console.log(bonus);
+    console.log(mood);
+}
+const output=sum (3,7);
+//console.log(bonus);
+//console.log(output);
